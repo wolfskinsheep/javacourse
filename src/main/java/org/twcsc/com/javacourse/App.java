0bc36244a2791c52b5733b8dc5c637bfbec4d219
wolfskinsheep <wolfskinsheep@gmail.com>
2024-09-02 11:01:09 +0800

    First add new files.

---
+++ src/main/java/org/twcsc/com/javacourse/App.java
@@ -0,0 +1,10 @@
+package org.twcsc.com.javacourse;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
